11bd8db54a30d66ee488cc45a9e01f0be4670db2
shihongyan <shy_1214@163.com>
2022-09-02 18:00:30 +0800

    新闻中心

--- src/views/news/index.vue
+++ src/views/news/index.vue
@@ -8,11 +8,25 @@
  * @LastEditTime: 2022-09-02 16:51:12
 -->
 <template>
-  <div class="container">news</div>
+  <div class="page-news">
+    <div class="f5">
+      <div class="layout">
+        <Title :title="'新闻中心'" />
+      </div>
+    </div>
+  </div>
 </template>
 
 <script setup>
 import { ref } from 'vue'
+import Title from '@/components/app-title.vue'
 </script>
 
-<style lang="less" scoped></style>
+<style lang="less" scoped>
+.f5 {
+  background-color: @backColor;
+}
+.layout {
+  .layout();
+}
+</style>
