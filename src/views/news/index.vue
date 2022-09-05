d65dade19d991c23cc907f3bfb78f5abf3d48774
shihongyan <shy_1214@163.com>
2022-09-05 10:32:24 +0800

    c

--- src/views/news/index.vue
+++ src/views/news/index.vue
@@ -4,14 +4,29 @@
  * @Author: 苏
  * @email: 1373842098@qq.com
  * @Date: 2022-09-02 16:51:12
- * @LastEditors: sj
- * @LastEditTime: 2022-09-02 16:51:12
+ * @LastEditors: linxi
+ * @LastEditTime: 2022-09-05 10:20:43
 -->
 <template>
   <div class="page-news">
     <div class="f5">
       <div class="layout">
         <Title :title="'新闻中心'" />
+        <el-row :gutter="70" justify="space-evenly">
+          <el-col :span="2">
+            <div class="time">
+              <p>8/24</p>
+            </div>
+          </el-col>
+          <el-col :span="22">
+            <div class="server">
+              <p>111</p>
+            </div>
+            <div class="server">
+              <p>111</p>
+            </div>
+          </el-col>
+        </el-row>
       </div>
     </div>
   </div>
@@ -28,5 +43,10 @@ import Title from '@/components/app-title.vue'
 }
 .layout {
   .layout();
+}
+.time {
+  height: 100%;
+  display: flex;
+  align-items: flex-end;
 }
 </style>
